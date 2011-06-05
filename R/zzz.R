a3d62cc88162b41dc9fc5aa70c02282b5191a690
mtmorgan@fhcrc.org <mtmorgan@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-06-05 17:39:33 +0000

    windows configuration and linux tweaks
    - use zlibbioc/usrlib/<arch>/ for all libraries
    - dyn.load dll in .onLoad
    - must Depends: zlibbioc on windows for DLL to be available at run time
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/zlibbioc@55998 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,11 @@
     function(libname, pkgname)
 {
     ## make DLL available for Windows; others use static library
-    if ("windows" == .Platform$OS.type)
-        library.dynam("zlib1bioc.dll", pkgname)
+    if ("windows" == .Platform$OS.type) {
+        dll <- file.path(libname, pkgname, "usrlib", .Platform$r_arch,
+                         "zlib1bioc.dll")
+        dyn.load(dll)
+    }
 }
 
 pkgconfig <-
@@ -11,17 +14,22 @@ pkgconfig <-
                "PKG_LIBS_shared"))
 {
     opt <- match.arg(opt)
+    incl <- system.file("include", package="zlibbioc")
+    pth <- system.file("usrlib", package="zlibbioc", mustWork=TRUE)
     if (nzchar(.Platform$r_arch)) {
-        pth <- system.file("libs", .Platform$r_arch, package="zlibbioc",
-                           mustWork=TRUE)
         arch <- sprintf("/%s", .Platform$r_arch)
     } else {
-        pth <- system.file("usrlib", package="zlibbioc", mustWork=TRUE)
         arch <- ""
     }
-    cat(list(PKG_CFLAGS='',
+    if ("windows" == .Platform$OS.type) {
+        PKG_LIBS_shared <-
+            sprintf('-L"%s%s" -lzlib1bioc', pth, arch)
+    } else {
+        PKG_LIBS_shared <-
+            sprintf('-L"%s%s" -Wl,-rpath,"%s%s" -lzbioc', pth, arch,
+                    pth, arch)
+    }
+    cat(list(PKG_CFLAGS=sprintf('-I"%s"', incl),
              PKG_LIBS_static=sprintf('"%s%s/libzbioc.a"', pth, arch),
-             PKG_LIBS_shared=sprintf(
-               '-L"%s%s" -Wl,-rpath,"%s%s" -lzbioc', pth, arch, pth,
-               arch))[[opt]])
+             PKG_LIBS_shared=PKG_LIBS_shared)[[opt]])
 }
